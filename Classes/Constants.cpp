6586f6e5d76b2c4ae279a727c51f00b3c3953fb8
Md Faizul Islam <36614551+mdfaizulislam@users.noreply.github.com>
2023-10-30 20:09:09 +0600

    added app constants

---
+++ Classes/Constants.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Md. Faizul Islam on 30/10/23.
+//
+
+#include "Constants.h"
+
+
+const char* Events::EVENT_JUMP = "event_jump";
---
+++ Classes/Constants.h
@@ -0,0 +1,20 @@
+//
+// Created by Md. Faizul Islam on 30/10/23.
+//
+
+#ifndef PROJ_ANDROID_CONSTANTS_H
+#define PROJ_ANDROID_CONSTANTS_H
+
+enum BgLayerIndex {
+    back = 1,
+    mid,
+    front
+};
+
+class Events {
+public:
+    static const char *EVENT_JUMP;
+};
+
+
+#endif //PROJ_ANDROID_CONSTANTS_H
