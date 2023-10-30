d3b82b766172e33bf2283940343c130c7dc93831
Md Faizul Islam <36614551+mdfaizulislam@users.noreply.github.com>
2023-10-30 20:08:29 +0600

    added configs

---
+++ Classes/configs/Config.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Md. Faizul Islam on 30/10/23.
+//
+
+#include "Config.h"
+
+const float JumpConfig::bgMoveX = -150.0f;
+const float JumpConfig::duration = 1.0f;
+const float JumpConfig::deltaXScaleFactorBack = 4.0;
+const float JumpConfig::deltaXScaleFactorMid = 2.0;
+const float JumpConfig::deltaXScaleFactorFront = 1.0;
+const float JumpConfig::jumpHeight = 100.0;
---
+++ Classes/configs/Config.h
@@ -0,0 +1,21 @@
+//
+// Created by Md. Faizul Islam on 30/10/23.
+//
+
+#ifndef PROJ_ANDROID_CONFIG_H
+#define PROJ_ANDROID_CONFIG_H
+
+
+
+class JumpConfig {
+public:
+    static const float bgMoveX;
+    static const float duration;
+    static const float deltaXScaleFactorBack;
+    static const float deltaXScaleFactorMid;
+    static const float deltaXScaleFactorFront;
+    static const float jumpHeight;
+};
+
+
+#endif //PROJ_ANDROID_CONFIG_H
